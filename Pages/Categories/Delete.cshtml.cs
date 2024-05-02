0aa70dce3d025b5e758341d9b06c8f535f3e4d0e
Alejandro <mariaeugenialopez456@mail.com>
2024-05-01 21:53:54 -0500

    Create Delete

---
+++ Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SupermarketWEB.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
